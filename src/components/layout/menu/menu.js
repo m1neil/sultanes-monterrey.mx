f076bd45c866e00b7bc9833013d5453f8e87cc6d
Alekseyev Eric <ericalex0207@gmail.com>
2025-10-15 15:28:44 +0300

    add: section promo

--- src/components/layout/menu/menu.js
+++ src/components/layout/menu/menu.js
@@ -1,16 +1,22 @@
 // Підключення функціоналу "Чортоги Фрілансера"
-import { addTouchAttr, bodyLockStatus, bodyLockToggle, FLS } from "@js/common/functions.js"
+import {
+	addTouchAttr,
+	bodyLockStatus,
+	bodyLockToggle,
+	FLS,
+} from '@js/common/functions.js'
 
 import './menu.scss'
 
 export function menuInit() {
-	document.addEventListener("click", function (e) {
+	document.addEventListener('click', function (e) {
 		if (bodyLockStatus && e.target.closest('[data-fls-menu]')) {
-			bodyLockToggle()
-			document.documentElement.toggleAttribute("data-fls-menu-open")
+			bodyLockToggle(300)
+			document.documentElement.toggleAttribute('data-fls-menu-open')
 		}
 	})
 }
 
-document.querySelector('[data-fls-menu]') ?
-	window.addEventListener('load', menuInit) : null
+document.querySelector('[data-fls-menu]')
+	? window.addEventListener('load', menuInit)
+	: null
