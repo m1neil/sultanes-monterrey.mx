3a2070ee6826e7f9c70ea153d935ee010b02c4df
Alekseyev Eric <ericalex0207@gmail.com>
2025-10-20 20:29:00 +0300

    add form in contacts page

--- src/components/forms/datepicker/datepicker.js
+++ src/components/forms/datepicker/datepicker.js
@@ -1,24 +1,23 @@
 // Підключення модуля
-import datepicker from 'js-datepicker';
+import datepicker from 'js-datepicker'
 
-import langs from "./_lang.json"
+import langs from './_lang.json'
 import './datepicker.scss'
 
 if (document.querySelector('[data-fls-datepicker]')) {
-	const LANG = 'ua' // en
+	const LANG = 'en' // en
 	const datePicker = datepicker('[data-fls-datepicker]', {
 		customDays: langs[LANG].week,
 		customMonths: langs[LANG].month,
 		overlayButton: langs[LANG].button,
 		overlayPlaceholder: langs[LANG].year,
 		startDay: 1,
+		dateSelected: new Date(),
 		formatter: (input, date, instance) => {
 			const value = date.toLocaleDateString()
 			input.value = value
 		},
-		onSelect: function (input, instance, date) {
-
-		}
-	});
-	window.flsDatepicker = datePicker;
+		onSelect: function (input, instance, date) {},
+	})
+	window.flsDatepicker = datePicker
 }
